49bb52813ddeae865029762830b2814cbdde960d
Sid Ferreira <sid@ferreiraz.com.br>
2019-09-05 21:07:09 -0300

    cleanup

--- ios/VideoGoogleDAI.h
+++ ios/VideoGoogleDAI.h
@@ -4,15 +4,11 @@
 #import "UIView+React.h"
 
 @import GoogleInteractiveMediaAds;
-//@class RCTVideo;
 
 @interface RCTVideoGoogleDAI : RCTView <RCTVideoDelegate>
 
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 
-- (void)requestStreamForSource:(NSDictionary *)source;
 - (AVPlayer *)didSetupPlayerWithPlayerItem:(AVPlayerItem *) playerItem withSource:(NSDictionary *) source;
-
-//- (RCTVideoGoogleDAI *)initWithRCTVideo:(RCTVideo *)rctVideo;
 
 @end
