d6313f52f60236ef097b0c5a77affefb2f53c07d
Sid Ferreira <143615+sidferreira@users.noreply.github.com>
2021-03-23 08:39:53 -0700

    Android implementation

--- android/src/main/java/com/reactlibrary/googleIMA/RNGoogleIMAPackage.java
+++ android/src/main/java/com/reactlibrary/googleIMA/RNGoogleIMAPackage.java
@@ -17,11 +17,6 @@ public class RNGoogleIMAPackage implements ReactPackage {
   }
 
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return null;
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.singletonList(new RNGoogleIMAManager(reactContext));
   }
